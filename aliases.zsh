57032aefe8d3f7f86bcafc82aaf77a62914d97e7
stevenmaharaj <stevesamah@gmail.com>
2026-05-28 07:52:56 +1000

    tmux

---
+++ aliases.zsh
@@ -0,0 +1,13 @@
+# Django Aliases
+alias dj='python manage.py'
+alias djrs='python manage.py runserver'
+alias djmm='python manage.py makemigrations'
+alias djmi='python manage.py migrate'
+alias djsh='python manage.py shell'
+alias sv="source .venv/bin/activate"
+alias dv="deactivate"
+
+alias dcp="docker compose"
+alias tn="tmux new -s"
+alias ta="tmux attach -t"
+alias tl="tmux ls"
